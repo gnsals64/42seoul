fe2071f8d8dd6c86d94c138722a1ac8f463391ae
chobinee <tnqls991213@icloud.com>
2023-09-24 18:38:11 +0900

    🔨fix: request method uri parsing

--- src/server/HandleData.cpp
+++ src/server/HandleData.cpp
@@ -64,7 +64,7 @@ int	Webserv::StartReceiveData(int len) {
 	//바디파싱
 	else if (eventData->request.getState() == BODY_READ)
 		ReadBody();
-	
+
 	return 0;
 }
 
@@ -92,7 +92,8 @@ int	Webserv::ReadHeader(void) {
 		}
 		else if (eventData->request.getHeaders().find("Transfer-Encoding") != std::string::npos)
 		{
-			if (eventData->request.Findrn0rn() == 1)
+			eventData->request.AddRNRNOneTime();
+			if (eventData->request.Findrn0rn(temp) == 1)
 				eventData->request.setState(READ_FINISH);
 			else
 				eventData->request.setState(BODY_READ);
@@ -105,6 +106,7 @@ int	Webserv::ReadHeader(void) {
 
 void	Webserv::ReadBody(void) {
 	eventData->request.BodyAppendVec(buffer);
+	std::string temp(buffer.begin(), buffer.end());
 	if (eventData->request.getHeaders().find("Content-Length") != std::string::npos)
 	{
 		size_t body_size = wit->checkContentLength(eventData->request.getHeaders());
@@ -112,14 +114,23 @@ void	Webserv::ReadBody(void) {
 			eventData->request.setState(READ_FINISH);
 	}
 	else if (eventData->request.getHeaders().find("Transfer-Encoding") != std::string::npos)
-		if (eventData->request.Findrn0rn() == 1)
+		if (eventData->request.Findrn0rn(temp) == 1)
 			eventData->request.setState(READ_FINISH);
 }
 
 void	Webserv::ReadFinish(void) {
 	wit->requestHeaderParse(eventData->request);
+	std::cout << "????" << std::endl;
 	if (eventData->request.getHeaders().find("Transfer-Encoding") != std::string::npos)
+	{
+		eventData->request.RemoveRNRNOneTime();
 		wit->chunkBodyParse(eventData->request, eventData->response);
+	}
+	std::vector <char> body = eventData->request.getBody();
+	std::string tmp_body(body.begin(), body.end());
+	// std::cout << "body222 = " << tmp_body << std::endl;
+	std::cout << "size = " << body.size() << " " << tmp_body.size() << std::endl;
+	std::cout << "!!!!" << std::endl;
 	ChangeEvent(change_list, curr_event->ident, EVFILT_READ, EV_DISABLE, 0, 0, curr_event->udata);
 	ChangeEvent(change_list, curr_event->ident, EVFILT_WRITE, EV_ENABLE, 0, 0, curr_event->udata);	//write 이벤트 발생
 	std::cout << "request header" << std::endl;
@@ -139,9 +150,12 @@ void    Webserv::MakeResponse(const Request &request) {
 	int location_idx = 0;
 	for(int i = 0; i < wit->get_locations().size(); i++)
 	{
-		if (request.getPath().find(wit->get_locations()[i].get_uri()) == 0) {
+		if (request.getPath().find(wit->get_locations()[i].get_uri()) != std::string::npos) {
 			location_idx = i;
-			break ;
+			if (wit->get_locations()[i].get_uri().length() == 1)
+				continue ;
+			else
+				break ;
 		}
 	}
 	if (request.getScheme().find("1.1") == std::string::npos)
@@ -154,7 +168,10 @@ void    Webserv::MakeResponse(const Request &request) {
     if (method == "GET" &&  limit_excepts[METHOD_GET])
         this->eventData->response.handleGET(eventData->request, wit->get_locations()[location_idx].get_index());
     else if (method == "POST" && limit_excepts[METHOD_POST])
+	{
+		std::cout << "POSTPOSTPOSTPOSTPOSTPOST" << std::endl;
         this->eventData->response.handlePOST(eventData->request);
+	}
     else if (method == "PUT" && limit_excepts[METHOD_PUT])
         this->eventData->response.handlePOST(eventData->request);
     else if (method == "DELETE" && limit_excepts[METHOD_DELETE])

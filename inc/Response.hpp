8edd37a038b0c51d7bc248880761d12426081bb8
ohhamma <judyham10634@naver.com>
2023-10-05 12:59:15 +0900

    :sparkles: feat: add needed http status codes

--- inc/Response.hpp
+++ inc/Response.hpp
@@ -68,17 +68,17 @@ class Response {
         {
             OK = 200,
             CREATED = 201,
-            ACCEPTED = 202,
             NO_CONTENT = 204,
+			MOVED_PERMANENTLY = 301,
             BAD_REQUEST = 400,
-            UNAUTHORIZED = 401,
-            FORBIDDEN = 403,
             NOT_FOUND = 404,
             METHOD_NOT_ALLOWED = 405,
-            CONTENT_TOO_LARGE = 413,
-            INTERNAL_SERVER_ERROR = 500,
+			LENGTH_REQUIRED = 411,
+            PAYLOAD_TOO_LARGE = 413,
+			URL_TOO_LONG = 414,
+			UNSUPPORTED_MEDIA_TYPE = 415,
             NOT_IMPLEMENTED = 501,
-            SERVICE_UNAVAILABLE = 503,
+            HTTP_VERSION_NOT_SUPPORTED = 505
         };
 };
 

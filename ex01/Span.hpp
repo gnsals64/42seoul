d45d67bf1447ce925843888e9159f35ccddab5e4
hunpark <gnsals64@gmail.com>
2023-12-29 23:42:19 +0900

    ✨feat : ex01

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,4 +1,21 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <iostream>
+# include <vector>
+
+class Span {
+private:
+	int max_store;
+	std::vector<int> v;
+public:
+	Span(unsigned int N);
+	~Span();
+	Span(const Span &cpy);
+	Span &operator=(const Span &cpy);
+	void addNumber(int num);
+	unsigned int shortestSpan();
+	unsigned int longestSpan();
+};
+
 #endif

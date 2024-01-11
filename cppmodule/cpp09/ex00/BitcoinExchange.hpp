ee8f5e8d2750060a4a57fe8d47e66cbe1327cdb5
hunpark <gnsals64@gmail.com>
2024-01-11 18:50:54 +0900

    ✨feat : ex00

---
+++ cppmodule/cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,17 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+
+class btc {
+private:
+
+public:
+	btc();
+	~btc();
+	btc(const btc &cpy);
+	btc& operator=(const btc &cpy);
+
+};
+
+#endif
